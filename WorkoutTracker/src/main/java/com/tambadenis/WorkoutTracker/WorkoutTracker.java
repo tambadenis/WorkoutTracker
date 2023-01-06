da88ea8ab7b7e5eef09942046b530f82c85f91ed
tambadenis <56196003+tambadenis@users.noreply.github.com>
2023-01-06 08:49:57 +0200

    Add files via upload

---
+++ WorkoutTracker/src/main/java/com/tambadenis/WorkoutTracker/WorkoutTracker.java
@@ -0,0 +1,13 @@
+package com.tambadenis.WorkoutTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutTracker {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutTracker.class, args);
+	}
+
+}
