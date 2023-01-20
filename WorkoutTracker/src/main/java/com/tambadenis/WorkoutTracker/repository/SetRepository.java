230b2f88cea20ca0c0ecf86cfbe85058aa8c6af0
tambadenis <56196003+tambadenis@users.noreply.github.com>
2023-01-20 10:35:17 +0200

    Delete WorkoutTracker/src directory

--- WorkoutTracker/src/main/java/com/tambadenis/WorkoutTracker/repository/SetRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tambadenis.WorkoutTracker.repository;
-
-import com.tambadenis.WorkoutTracker.model.Set;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SetRepository extends JpaRepository<Set, Long> {}
